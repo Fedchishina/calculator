73d2d58c04de875a5f7d4f7a7374aaca91d04ade
Viktoria Fedchishina <viktoria.fedchishina@semrush.com>
2023-01-23 10:19:43 +0100

    add another validate checks for input data

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # calculator
+
 application for calculation various examples
 
 # How it works
+
 1) Build application `go build main.go`
 2) Run application `go run main.go`
 3) Enter your example like "2+2" and you will see the answer
--- application/entity/infixExpr.go
+++ application/entity/infixExpr.go
@@ -1,7 +1,9 @@
 package entity
 
 import (
+	"calculator/application/stack"
 	"calculator/application/validator"
+	"strings"
 )
 
 type InfixExpr string
@@ -9,17 +11,27 @@ type InfixExpr string
 func (i *InfixExpr) Validate(v *validator.Validator, value string) {
 	v.Check(value != "", "the input data is empty. Enter data please")
 	v.Check(symbolsValid(v, value), "the input data is invalid")
+	v.Check(checkBalance(value), "the input data is invalid: problem with parentheses")
 }
 
 func symbolsValid(v *validator.Validator, input string) bool {
-	//validSymbols := getValidSymbols()
-	//for _, s := range strings.Split(input, "") {
-	//	if ! Contains(validSymbols, s) {
-	//		v.AddError("Wrong symbol: " + s)
-	//	}
-	//}
+	validSymbols := getValidSymbols()
+	for _, s := range strings.Split(input, "") {
+		if !contains(validSymbols, s) {
+			v.AddError("Wrong symbol: " + s)
+		}
+	}
 
 	return v.Valid()
+}
+
+func contains(symbols []string, s string) bool {
+	for _, n := range symbols {
+		if s == n {
+			return true
+		}
+	}
+	return false
 }
 
 func getValidSymbols() []string {
@@ -29,4 +41,27 @@ func getValidSymbols() []string {
 		"(", ")",
 		"^",
 	}
+}
+
+func checkBalance(input string) bool {
+	st := stack.Stack{}
+
+	for _, char := range input {
+		switch char {
+		case '(':
+			st.Push(string(char))
+		case ')':
+			top := st.Top()
+			if top == "(" {
+				st.Pop()
+			} else {
+				return false
+			}
+		}
+	}
+
+	if st.IsEmpty() {
+		return true
+	}
+	return false
 }
